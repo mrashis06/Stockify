635e2dd61a1e2a5858356a8855ca72e2ad8d34c6
Mr Ashis <mrashis0603@gmail.com>
2025-09-23 15:37:19 +0000

    why october month remove that only present moth and previous

--- src/app/dashboard/reports/page.tsx
+++ src/app/dashboard/reports/page.tsx
@@ -233,7 +233,7 @@ export default function ReportsPage() {
                         defaultMonth={date?.from}
                         selected={date}
                         onSelect={setDate}
-                        numberOfMonths={2}
+                        numberOfMonths={1}
                     />
                     </PopoverContent>
                 </Popover>
