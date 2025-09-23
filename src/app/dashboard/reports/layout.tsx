721637275e81dd8d8e938ed39e09fd33ed53552e
Mr Ashis <mrashis0603@gmail.com>
2025-09-23 19:25:30 +0000

    Try fixing this error: `Console Error: The keys of `searchParams` were a

--- src/app/dashboard/reports/layout.tsx
+++ src/app/dashboard/reports/layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 
-export default function ReportsLayout({ children }: { children: ReactNode }) {
+export default function ReportsLayout({ children, params, searchParams }: { children: ReactNode; params: { slug: string }; searchParams?: { [key: string]: string | string[] | undefined }; }) {
   return (
     <div className="flex-1 p-4 md:p-8">
       <main>{children}</main>
--- src/app/dashboard/settings/layout.tsx
+++ src/app/dashboard/settings/layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 
-export default function SettingsLayout({ children }: { children: ReactNode }) {
+export default function SettingsLayout({ children, params, searchParams }: { children: ReactNode; params: { slug: string }; searchParams?: { [key: string]: string | string[] | undefined }; }) {
   return (
     <div className="flex-1 p-4 md:p-8">
       {children}
