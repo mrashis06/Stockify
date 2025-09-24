5639fec20b24cff1b0c9357e1b69f1d8fb4d1229
Mr Ashis <mrashis0603@gmail.com>
2025-09-24 09:05:49 +0000

    bro we already created project man

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,9 +10,9 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   "projectId": "studio-8867609365-e1454",
   "appId": "1:606856816045:web:b74c33836dfe99597b2d24",
+  "storageBucket": "studio-8867609365-e1454.appspot.com",
   "apiKey": "AIzaSyBllYuCJ_7e7QXzWkmStlTZOACnKPMMYkc",
   "authDomain": "studio-8867609365-e1454.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "606856816045"
 };
 
