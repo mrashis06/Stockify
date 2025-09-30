94e30a0a3c0f93c428ba7a0945abb90304a677c4
Mr Ashis <mrashis0603@gmail.com>
2025-09-30 06:22:48 +0000

    update the calender in performance for custome date selection it fixed f

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -26,9 +26,9 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium hidden",
+        caption_label: "text-sm font-medium",
         caption_dropdowns: "flex justify-center gap-2 items-center",
-        nav: "hidden",
+        nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
