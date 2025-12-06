a71bf52514c5788718cbe18747c6deae3adc4075
Mr Ashis <mrashis0603@gmail.com>
2025-12-06 09:26:18 +0000

    ??

--- next.config.ts
+++ next.config.ts
@@ -36,4 +36,6 @@ const nextConfig: NextConfig = {
 export default withPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
 })(nextConfig);
